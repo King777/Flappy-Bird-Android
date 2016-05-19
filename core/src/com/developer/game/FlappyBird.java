6d381955ca7b901420e0215d7df6045fe7e2f0a2
developerking <kinglo@developerking.com>
2016-05-19 12:53:16 -0400

    Add collision detection code and log collsion to Gdx.app.log

--- core/src/com/developer/game/FlappyBird.java
+++ core/src/com/developer/game/FlappyBird.java
@@ -8,6 +8,7 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.math.Circle;
+import com.badlogic.gdx.math.Intersector;
 import com.badlogic.gdx.math.Rectangle;
 
 import java.util.Random;
@@ -123,6 +124,11 @@ public class FlappyBird extends ApplicationAdapter {
 		for (int i = 0; i < numberOfTubes; i++) {
 			shapeRenderer.rect(tubeX[i], Gdx.graphics.getHeight() / 2 + gap / 2 + tubeOffset[i], topTube.getWidth(), topTube.getHeight());
 			shapeRenderer.rect(tubeX[i], Gdx.graphics.getHeight() / 2 - gap / 2 - bottomTube.getHeight() + tubeOffset[i], bottomTube.getWidth(), bottomTube.getHeight());
+
+			if (Intersector.overlaps(birdCircle, topTubeRectangles[i]) || Intersector.overlaps(birdCircle, bottomTubeRectangles[i])){
+				Gdx.app.log("Collision","Yes");
+			}
+
 		}
 
 		shapeRenderer.end();
